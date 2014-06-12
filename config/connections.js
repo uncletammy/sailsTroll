85744e60d249fea9d91ed00ca6562127938c11d0
uncletammy <uncletammy@gmail.com>
2014-06-12 13:20:09 -0500

    changed from local to remote mongo

--- config/connections.js
+++ config/connections.js
@@ -52,8 +52,8 @@ module.exports.connections = {
   //
  someMongodbServer: {
     adapter   : 'sails-mongo',
-    database : 'E4sonabe'
-    // url: local.mongo.url
+    // database : 'E4sonabe'
+    url: local.mongo.url
   },
 
   // PostgreSQL is another officially supported relational database.
