a6c2804856c73b7a5c73056d8ced23898c16aac4
uncletammy <uncletammy@gmail.com>
2014-06-12 15:08:56 -0500

    removed lodash dependency from irc config

--- config/irc.js
+++ config/irc.js
@@ -1,79 +1,4 @@
-var _ = require('lodash');
-
-// var processMessages = _.each(messages, function(oneMessage){
-// 	var userID = oneMessage.sender;
-
-// 	var getSender = _.find(users,{id:userID});
-
-// 	// get links in message text
-// 	var linksFound = oneMessage.text.match(/(https{0,1}:\/\/[:?=#!.\w\d\/_-]+)/ig)
-// 	var mappedLinks = _.map(linksFound,function(oneLink){
-// 		var ensureUniqueLink = _.find(linksToSave,{linktext:oneLink})
-// 		// check that we havnt already pushed the link to our array.  If so, just add
-// 		// add the relevent info to it.  Otherwise, parse it and push it to our array
-// 		if (ensureUniqueLink){
-// 			// console.log(oneLink,'has already been posted by',ensureUniqueLink.postedby);
-// 			linksToSave = _.without(linksToSave,ensureUniqueLink);
-// 			modifiedLink = ensureUniqueLink;
-// 			modifiedLink.postedby.push(getSender.lcnick);
-// 			modifiedLink.postedby = _.unique(modifiedLink.postedby);
-// 			modifiedLink.postedin.push(oneMessage['_id']['$oid']);
-// 		} else {
-// 			var indexOfLinkStart = oneLink.indexOf('://');
-// 			// Chop off protocol and split at '/'
-// 			var splitLinkPath = oneLink.split(/https{0,1}:\/\//i)[1].split('/');
-// 			if (splitLinkPath.length)
-// 				getLinkDomain = splitLinkPath[0];
-// 			else
-// 				getLinkDomain = splitLinkPath.join('');
-
-// 			// console.log('Link Domain:',getLinkDomain)
-// 			var modifiedLink = {
-// 				linktext:oneLink,
-// 				domain:getLinkDomain,
-// 				postedby:[getSender.lcnick],
-// 				postedin:[oneMessage['_id']['$oid']]
-// 			};			
-// 		}
-// 		return modifiedLink
-// 	})
-
-
-// 	linksToSave = linksToSave.concat(mappedLinks);
-// 	// Get other users mentioned in message text
-// 	var getMessageWords = _.unique(oneMessage.text.toLowerCase().replace(/[^\w ]/ig,'').split(' '));
-// 	var userMentions = _.intersection(getMessageWords,_.pluck(users,'lcnick'));
-// 	var filterMentions = _.filter(userMentions,function(thisMention){
-// 		return thisMention.length >= 4
-// 	})
-// 	oneMessage.usermentions = filterMentions;
-// 	oneMessage.sender = getSender.lcnick;
-
-// 	return oneMessage
-// });
-
-  	// text: 'STRING',
-  	// // No two users will post the exact same message (with the same IDs)
-  	// sender:{
-  	// 	model: 'user'
-  	// },
-   //  channel: {
-   //    model: 'channel'
-   //  },
-   //  links:{
-   //    collection: 'link',
-   //    via: 'postedin',
-   //    dominant:false
-   //  },
-   //  usermentions:{
-   //    collection: 'user',
-   //    via: 'mentionedin',
-   //    dominant:true
-   //  }
-
-
-
-
+// var _ = require('lodash');
 
 var onIRCMessage = function(from, to, message){
 
@@ -89,7 +14,6 @@ var onIRCMessage = function(from, to, message){
 		console.log('Create callback fired and',m.id,'saved')
 	})
 
-	// console.log('Sails is undefined:',_.isUndefined(sails))
 /*
 	var theMessage = message.toLowerCase().replace(/[:,!]/g,' ');
 
